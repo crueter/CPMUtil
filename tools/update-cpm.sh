38049f191a9585509e26068cccbf7bd8230cd9fc
crueter <crueter@eden-emu.dev>
2025-10-07 17:27:22 -0400

    real update-cpm.sh
    Signed-off-by: crueter <crueter@eden-emu.dev>

--- tools/update-cpm.sh
+++ tools/update-cpm.sh
@@ -3,12 +3,14 @@
 # SPDX-FileCopyrightText: Copyright 2025 crueter
 # SPDX-License-Identifier: GPL-3.0-or-later
 
-# Change CMakeModules to wherever you store external/Find modules
-MODULES_DIR=CMakeModules
-BASE_URL="https://git.crueter.xyz/CMake/CPMUtil/raw/branch/master"
+# updates CPMUtil, its docs, and related tools from the latest release
 
-# You may optionally choose to use CPMUtil.cmake releases when they are available
-# These will also update tooling and documentation.
-# OR use tags when they are available
-wget -O "$MODULES_DIR/CPM.cmake" "$BASE_URL/CPM.cmake"
-wget -O "$MODULES_DIR/CPMUtil.cmake" "$BASE_URL/CPMUtil.cmake"
+if command -v zstd > /dev/null; then
+    EXT=tar.zst
+else
+    EXT=tar.gz
+fi
+
+wget "https://git.crueter.xyz/CMake/CPMUtil/releases/download/continuous/CPMUtil.$EXT"
+tar xf "CPMUtil.$EXT"
+rm "CPMUtil.$EXT"
