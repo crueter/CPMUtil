c3fe23f72fe99eadcd6944fceffbf854b3322072
crueter <crueter@eden-emu.dev>
2026-06-27 16:18:42 -0400

    oops
    Signed-off-by: crueter <crueter@eden-emu.dev>

---
+++ scripts/hash.cmake
@@ -0,0 +1,31 @@
+#!/usr/bin/env -S cmake -P
+
+# SPDX-FileCopyrightText: Copyright 2026 crueter
+# SPDX-License-Identifier: LGPL-3.0-or-later
+
+cmake_minimum_required(VERSION 3.31)
+
+list(APPEND CMAKE_MODULE_PATH ${CMAKE_CURRENT_LIST_DIR})
+include(utils)
+
+parse_key(discord-rpc)
+
+package_hash()
+
+option(CORRECT_HASH "Correct hash if it's mismatched" OFF)
+
+message(STATUS "correct: ${CORRECT_HASH}")
+
+if (pkg_hash STREQUAL hash)
+    echo("Hashes match")
+else()
+    echo_error("Hash mismatch")
+    echo_error("Expected: ${hash}")
+    echo_error("Got:      ${pkg_hash}")
+
+    if (CORRECT_HASH)
+        correct_package_hash()
+    else()
+        cmake_language(EXIT 1)
+    endif()
+endif()
